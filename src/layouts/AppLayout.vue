28ce2697243527e89b3ad014e22de9b7a2040b63
roberto-007 <al76700@alunos.utad.pt>
2026-05-13 15:30:29 +0100

    Remove sidebar logout button for authenticated users in AppLayout.vue to streamline UI for guests. This change enhances the user experience by simplifying the navigation options based on authentication status.

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -348,12 +348,6 @@ function isActive(path: string) {
             </RouterLink>
           </div>
         </nav>
-        <div v-if="authStore.isAuthenticated" class="sidebar-footer">
-          <button type="button" class="btn-sidebar btn-logout" @click="logout">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"/><polyline points="16 17 21 12 16 7"/><line x1="21" x2="9" y1="12" y2="12"/></svg>
-            <span class="sidebar-label">Sair</span>
-          </button>
-        </div>
         <div v-else class="sidebar-guest">
           <RouterLink to="/entrar" class="sidebar-link">Entrar</RouterLink>
           <RouterLink to="/registar" class="sidebar-link btn-register">Registar</RouterLink>
